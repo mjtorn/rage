609fd3e35c7e54e27f05eaecee18c01ca23d1d3b
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2015-12-12 22:51:57 +0900

    eage theme/look - update it to be simpler along with existing changes
    this up[dates most icons etc.

--- data/themes/images/Makefile.mk
+++ data/themes/images/Makefile.mk
@@ -1,58 +1,39 @@
 THEME_IMGS = \
-data/themes/images/shadow_circle_horiz.png \
-data/themes/images/shadow_circle_vert.png \
-data/themes/images/vg1.png \
-data/themes/images/vg2.png \
-data/themes/images/bevel_dark_out.png \
-data/themes/images/bg_bevel.png \
-data/themes/images/bg_glint.png \
-data/themes/images/bg_shine.png \
 data/themes/images/dimmer.png \
-data/themes/images/glow_exclam.png \
 data/themes/images/icon_arrow_right.png \
-data/themes/images/icon_close2.png \
 data/themes/images/icon_close.png \
 data/themes/images/icon_eject.png \
+data/themes/images/icon_exclamation.png \
 data/themes/images/icon_forward.png \
-data/themes/images/icon_fullscreen2.png \
 data/themes/images/icon_fullscreen.png \
 data/themes/images/icon_mute.png \
-data/themes/images/icon_next2.png \
 data/themes/images/icon_next.png \
-data/themes/images/icon_pause2.png \
 data/themes/images/icon_pause.png \
-data/themes/images/icon_play2.png \
 data/themes/images/icon_play.png \
-data/themes/images/icon_prev2.png \
 data/themes/images/icon_prev.png \
 data/themes/images/icon_refresh.png \
 data/themes/images/icon_rewind.png \
-data/themes/images/icon_spanner2.png \
 data/themes/images/icon_spanner.png \
 data/themes/images/icon_stop.png \
-data/themes/images/icon_unfullscreen2.png \
 data/themes/images/icon_unfullscreen.png \
-data/themes/images/icon_volume2.png \
 data/themes/images/icon_volume.png \
 data/themes/images/pm_fill.png \
-data/themes/images/pm_overlay.png \
-data/themes/images/pm_shadow.png \
 data/themes/images/r-0.png \
 data/themes/images/r-1.png \
 data/themes/images/r-2.png \
 data/themes/images/r-3.png \
 data/themes/images/r-4.png \
-data/themes/images/tv-bg.png \
-data/themes/images/tv-over.png \
-data/themes/images/win_glow.png \
-data/themes/images/win_shadow.png \
-data/themes/images/posglow.png \
+data/themes/images/shadow_circle_horiz.png \
+data/themes/images/shadow_circle_vert.png \
 data/themes/images/speaker-base.png \
 data/themes/images/speaker-bevel-hi.png \
 data/themes/images/speaker-bevel-lo.png \
 data/themes/images/speaker-over.png \
 data/themes/images/speaker-shadow.png \
-data/themes/images/vgrad-dark-inv.png \
-data/themes/images/vgrad-dark-inv-trans.png
+data/themes/images/tv-bg.png \
+data/themes/images/tv-over.png \
+data/themes/images/vg1.png \
+data/themes/images/vg2.png \
+data/themes/images/win_shadow.png
 
 EXTRA_DIST += $(THEME_IMGS)
