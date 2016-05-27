43d1998c5f3f5285c96da4fd26f281ff6bb88dc7
Carsten Haitzler (Rasterman) <raster@rasterman.com>
2016-05-27 12:31:57 +0900

    Revert "improve build system to use only one makefile"
    This reverts commit 25ffdc470cdd28d4c55782563b833151641a0356.
    
    go back to good 'old subdir build. it's just too much of a pain having
    to always build from a toplevel dir. see all the issues with efl doing
    this.

--- data/desktop/Makefile.mk
+++
@@ -1,5 +0,0 @@
-desktopdir = $(datadir)/applications
-desktop_DATA = \
-data/desktop/rage.desktop
-
-EXTRA_DIST += $(desktop_DATA)
--- data/icons/Makefile.mk
+++
@@ -1,5 +0,0 @@
-icondir = $(datadir)/icons
-icon_DATA = \
-data/icons/rage.png
-
-EXTRA_DIST += $(icon_DATA)
